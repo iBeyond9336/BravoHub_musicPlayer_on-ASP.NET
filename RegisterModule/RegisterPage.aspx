daf80420a061587db3360168d45d45aaf7239557
Edwin A. Collazos <eacl.831@gmail.com>
2023-12-04 23:11:48 -0500

    [Milestone3]: New RegisterModule and code refactor for the LoginPage.aspx

---
+++ RegisterModule/RegisterPage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegisterPage.aspx.cs" Inherits="BravoHub.RegiterPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Register Page</title>
+    <link href="registerStyles.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Type your new username your username"></asp:Label>
+        <input id="RegisterUsername" type="text" placeholder="i.e. " runat="server"/>
+        <asp:Label ID="Label2" runat="server" Text="Type your new password"></asp:Label>
+        <input id="RegisterPassword1" type="password" placeholder="password" runat="server"/>
+        <asp:Label ID="Label3" runat="server" Text="Confirm your password"></asp:Label>
+        <input id="RegisterPassword2" type="password" placeholder="confirm password" runat="server"/>
+        <div id="ErrorMsg" runat="server"></div>
+
+        <asp:Button ID="Button1" runat="server" Text="Sign Up!" OnClick="Button1_Click" />
+    </form>
+</body>
+</html>
---
+++ RegisterModule/RegisterPage.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BravoHub {
+    public partial class RegiterPage : System.Web.UI.Page {
+        private const string LOGIN_PAGE_URL = "../LoginPage.aspx";
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            bool result = RegisterNewUser();
+            if (result) {
+                // Register the user in the DB
+                ErrorMsg.InnerText = "Registered!";
+                Button1.Text = "Go back to Login Page";
+                return;
+            }
+
+            // Otherwise, display the error
+            ErrorMsg.InnerText = "Oops something happen";
+        }
+
+        private bool RegisterNewUser() {
+            if(Button1.Text == "Go back to Login Page") {
+                // go back to Login Page
+                Response.Redirect(LOGIN_PAGE_URL);
+                return true;
+            }
+
+            if (RegisterUsername.Value == string.Empty) {
+                return false;
+            } else if (RegisterPassword1.Value == string.Empty) {
+                return false;
+            } else if (RegisterPassword1.Value != RegisterPassword2.Value) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
